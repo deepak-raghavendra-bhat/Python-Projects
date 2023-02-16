5d3e186570c5970124838e14e83894e621a02bf9
deepakraghavendrabhat <deepak.raghavendra.bhat@outlook.com>
2023-02-16 08:39:15 +0530

    Algo testing

---
+++ leetcode/2562.find-the-array-concatenation-value.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=2562 lang=python3
+#
+# [2562] Find the Array Concatenation Value
+#
+
+# @lc code=start
+class Solution:
+    def findTheArrayConcVal(self, nums: List[int]) -> int:
+        
+# @lc code=end
+
